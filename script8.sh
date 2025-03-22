b68d8d9668bde63d8c7f28376eda1789c9ad4d22
ytrqua3 <suensenho@gmail.com>
2025-03-22 04:16:56 +0000

    finish with good output

--- script8.sh
+++ script8.sh
@@ -1,6 +1,29 @@
-fucntion readAnchor(){
-	curl $1 | sed '/<a/s//'
+function getLinks(){
+	curl -s $1 | grep -E -o '<a[^>]*>' | sed -E '/<a(.*)href=\"([^\"]*)\"(.*)$/s//http:\/\/localhost:8000\/\2/'
+}
+pagesVisited=()
+
+function crawl(){
+	links=$(mktemp)
+	getLinks $1 >$links
+	while read link
+	do
+		isNewPage=0
+		for page in "${pagesVisited[@]}"
+		do
+			if [ "$link" == "$page" ]
+			then
+				isNewPage=1
+			fi
+		done
+		if [ "$isNewPage" == "0" ]
+		then
+			pagesVisited+=($link)
+			crawl $link
+		fi
+	done <$links
 }
 
-readAnchor $1
-cat site/index.html | sed -r -n '/<a([^>]*)>(.*)<\/a>/s//\1/pg'
+crawl $1
+
+echo -n "${pagesVisited[@]}" | tr " " "\n" | sort
